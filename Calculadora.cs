48a29f794941226d9c280ff9f252dc99049293ae
Gustavo De Souza <74631630+Gustavsouza@users.noreply.github.com>
2023-01-12 21:14:01 -0300

    Initial commit

---
+++ Calculadora.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraBasica
+{
+    public class Calculadora
+    {
+        public string fatorConta;
+        public double fator1 { get; set; }
+        public double fator2 { get; set; }
+        public double resultado { get; set; }
+
+        public Calculadora(double fator1, string fatorConta, double fator2)
+        {
+            this.fatorConta = fatorConta;
+            this.fator1 = fator1;
+            this.fator2 = fator2;
+            this.resultado = resultado;
+
+         
+                if (fatorConta.Equals("*"))
+                {
+                    this.resultado = multiplicacao();
+                    Console.WriteLine(resultado);
+  
+                }
+                else if (fatorConta.Equals("-"))
+                {
+                    this.resultado = subtracao();
+                    Console.WriteLine(resultado);
+                }
+                else if (fatorConta.Equals("/"))
+                {
+                    this.resultado = divisao();
+                    Console.WriteLine(resultado);
+                }
+                else if (fatorConta.Equals("+"))
+                {
+                    this.resultado = soma();
+                    Console.WriteLine(resultado);
+                }
+                else
+                {
+                    Console.WriteLine("Valor do sinal Invalido");
+                }
+            
+        }
+        /*Aqui são os métodos*/
+        public double soma()
+        {
+            var resultado = this.fator1 + this.fator2;
+            return resultado;
+
+        }
+        public double subtracao()
+        {
+            var resultado = this.fator1 - this.fator2;
+            return resultado;
+        }
+        public double divisao()
+        {
+            var resultado = this.fator1 / this.fator2;
+            return resultado;
+
+        }
+        public double multiplicacao()
+        {
+    
+             resultado = this.fator1 * this.fator2;
+            return resultado;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿
+using CalculadoraBasica;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Calculadora!");
+        #region
+        //    Console.WriteLine("Calculadora!");
+        //    double resultado = 0;
+        //    while (true)
+        //    {
+        //        try { 
+        //            Console.WriteLine("Digite um valor: ");
+        //            double valor1 = double.Parse(Console.ReadLine());
+        //            Console.WriteLine("Digite um valor: ");
+        //            double valor2 = double.Parse(Console.ReadLine());
+
+
+        //        if (valor1 >=0 || valor2 >=0)
+        //        {
+        //            resultado += valor1 + valor2;
+        //            Console.WriteLine($"Resultado: {resultado}");
+        //        }
+
+        //    }catch(Exception ex)
+        //        {
+        //            Console.WriteLine("Estamos finalizando, dado de entrada invalido. ");
+        //            Console.WriteLine($"Resultado final: {resultado}");
+        //            Console.WriteLine(ex);
+        //            break;
+        //        }
+        //}
+        #endregion
+
+        List<Calculadora> ListaDeCalculos = new List<Calculadora>();
+        ChamadaDigitar();
+        void ChamadaDigitar()
+        {
+            /*Aqui estou criando uma lista do tipo calculadora*/
+
+            while (true)
+            {
+                try/*Tenta fazer isso*/
+                {
+
+                    Console.WriteLine("Digite um valor: ");
+                    var fator1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o sinal: ");
+                    var fatorConta = Console.ReadLine();
+                    Console.WriteLine("Digite um valor: ");
+                    var fator2 = double.Parse(Console.ReadLine());
+                    Calculadora conta = new Calculadora(fator1, fatorConta, fator2);
+                    ListaDeCalculos.Add(conta);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Finalizando.!.!.!");
+                    Console.WriteLine("Passou um valor fora do pedido");
+                    Console.WriteLine($"Cálculos já feitos: ");
+                    break;
+                }
+            }
+            PercorreArray();
+        }
+        void PercorreArray()
+        {
+            foreach (var item in ListaDeCalculos)
+            {
+                if (ListaDeCalculos.Count <= 0)
+                {
+                    Console.WriteLine("Array vazio");
+                }
+                else
+                {
+                    Console.WriteLine($"{item.fator1} {item.fatorConta} {item.fator2} = {item.resultado}");
+                    var caminhoDoArquivo = "ContasEmJason.txt";
+                    using (var fluxoDeArquivo = new FileStream(caminhoDoArquivo, FileMode.Append))
+                    {
+                        using (var salva = new StreamWriter(fluxoDeArquivo))
+
+                        {
+                            salva.WriteLine($"{item.fator1} {item.fatorConta} {item.fator2} = {item.resultado}");
+                            salva.Flush();
+                        }
+                    }
+
+            }
+        }
+
+    }
+
+
+
+}
+}
